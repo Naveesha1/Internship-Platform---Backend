0a66ee6d4d35191b61a56d8a9191d0db8c46852f
Manoj99 <ekanayakehgnk.21@uom.lk>
2025-02-07 23:18:24 +0530

    company internship preview
    company can see the internships created by them

--- routes/companyRoute.js
+++ routes/companyRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { companyProfileController,getCompanyController } from '../controllers/company/companyController.js';
+import { companyProfileController,getCompanyController, getCompanySpecificInternshipController } from '../controllers/company/companyController.js';
 import { createInternshipController } from '../controllers/internshipController.js';
 
 const companyRouter = express.Router();
@@ -7,6 +7,7 @@ const companyRouter = express.Router();
 companyRouter.post("/createprofile",companyProfileController);
 companyRouter.post("/createIntern",createInternshipController);
 companyRouter.post("/getCompany",getCompanyController);
+companyRouter.post("/allInternships",getCompanySpecificInternshipController);
  
 
 export default companyRouter;
