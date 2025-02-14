10b5a5304bcb852a2b8db038dfe74dd88be19c4f
Manoj99 <ekanayakehgnk.21@uom.lk>
2025-02-15 00:29:39 +0530

    company and student data fetching, updating parts updated
    companyController,
    internshipController,
    studentController,
    applyInternshipModel,
    companyRoute

--- models/student/applyInternshipModel.js
+++ models/student/applyInternshipModel.js
@@ -1,17 +1,20 @@
 import mongoose from "mongoose";
 
 const applicationSchema = new mongoose.Schema({
-    userName: { type: String, required: true },
-    userEmail: { type: String, required: true },
-    userCv: { type: String, required:true },
-    position: { type: String, required: true },
-    companyName: { type: String },
-    companyEmail: { type: String },
-    internshipId: { type: String },
-    status: { type: Boolean, default: false },
-    date: { type: String, default: () => new Date().toISOString().split('T')[0] },
+  userName: { type: String, required: true },
+  userEmail: { type: String, required: true },
+  userCv: { type: String, required: true },
+  userGpa: { type: String },
+  position: { type: String, required: true },
+  companyName: { type: String, required: true },
+  companyEmail: { type: String, required: true },
+  companyRegisteredEmail: { type: String },
+  internshipId: { type: String },
+  status: { type: Boolean, default: null },
+  date: { type: String, default: () => new Date().toISOString().split("T")[0] },
 });
 
 const applyInternshipModel =
-    mongoose.models.application || mongoose.model("application", applicationSchema);
+  mongoose.models.application ||
+  mongoose.model("application", applicationSchema);
 export default applyInternshipModel;
--- routes/companyRoute.js
+++ routes/companyRoute.js
@@ -1,13 +1,20 @@
-import express from 'express'
-import { companyProfileController,getCompanyController, getCompanySpecificInternshipController } from '../controllers/company/companyController.js';
-import { createInternshipController } from '../controllers/internshipController.js';
+import express from "express";
+import {
+  companyProfileController,
+  getCompanyController,
+  getCompanySpecificInternshipController,
+  getApplicantsController,
+  updateCvStatusController,
+} from "../controllers/company/companyController.js";
+import { createInternshipController } from "../controllers/internshipController.js";
 
 const companyRouter = express.Router();
 
-companyRouter.post("/createprofile",companyProfileController);
-companyRouter.post("/createIntern",createInternshipController);
-companyRouter.post("/getCompany",getCompanyController);
-companyRouter.post("/allInternships",getCompanySpecificInternshipController);
- 
+companyRouter.post("/createprofile", companyProfileController);
+companyRouter.post("/createIntern", createInternshipController);
+companyRouter.post("/getCompany", getCompanyController);
+companyRouter.post("/allInternships", getCompanySpecificInternshipController);
+companyRouter.post("/getApplicants", getApplicantsController);
+companyRouter.put("/updateStatus", updateCvStatusController);
 
 export default companyRouter;
