e7c335d2d554f378262ae4dca09a3d5cb120ddec
Manoj99 <110526623+Naveesha1@users.noreply.github.com>
2025-09-01 10:05:38 +0530

    gitignore update

--- config/db.js
+++ config/db.js
@@ -1,21 +1,18 @@
-//mongodb+srv://manoj:<db_password>@testdatabase.e058spu.mongodb.net/?retryWrites=true&w=majority&appName=testDatabase
-//mongodb+srv://manoj:<db_password>@internshipnexus.or16u.mongodb.net/?retryWrites=true&w=majority&appName=InternshipNexus
-// mongodb+srv://naveesha4:naveesha2001@internship-nexus.ysrnv67.mongodb.net/?retryWrites=true&w=majority&appName=Internship-nexus
-
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const connectDB = async () => {
-    try {
-        await mongoose.connect("mongodb+srv://naveesha4:naveesha2001@internship-nexus.ysrnv67.mongodb.net/?retryWrites=true&w=majority&appName=Internship-nexus");
-        console.log("DB connected");
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("DB connected");
 
-        // Listen for connection lost events
-        mongoose.connection.once("disconnected", () => {
-            console.log("DB disconnected");
-        });
-        
-    } catch (error) {
-        console.error("DB connection error:", error);
-    }
+    // Listen for connection lost events
+    mongoose.connection.once("disconnected", () => {
+      console.log("DB disconnected");
+    });
+  } catch (error) {
+    console.error("DB connection error:", error);
+  }
 };
-
